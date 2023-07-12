b60848b360b09cb291a18d69643af381fa910f13
hanyujin <hanyujin32432@gmail.com>
2023-07-12 23:12:12 +0900

    정보 은닉 test

---
+++ StudentTest.java
@@ -0,0 +1,10 @@
+package hiding;
+
+public class StudentTest{
+  public static void main(String[]args){
+    studentOne.setStudentName="김도영";
+
+    System.out.printLn(studentOne.getStudentName());
+  }
+
+}
